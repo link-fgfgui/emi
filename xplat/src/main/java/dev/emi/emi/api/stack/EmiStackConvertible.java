feeb9074bc620273ceba33c6e5125a4db8de409e
link-fgfgui <fgfgui_link@outlook.com>
2026-05-12 23:33:00 +0800

    1

--- xplat/src/main/java/dev/emi/emi/api/stack/EmiStackConvertible.java
+++ xplat/src/main/java/dev/emi/emi/api/stack/EmiStackConvertible.java
@@ -2,9 +2,6 @@ package dev.emi.emi.api.stack;
 
 import org.jetbrains.annotations.ApiStatus;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
 /**
  * Represents a type that can be implicitly converted into an EmiStack.
  */
@@ -16,7 +13,7 @@ public interface EmiStackConvertible {
 	 *  If the type has an amount of its own, it will be used.
 	 *  Otherwise, it'll use the default amount for the stack type.
 	 */
-	@Environment(EnvType.CLIENT)
+
 	@ApiStatus.Experimental
 	default EmiStack emi() {
 		throw new IllegalStateException();
@@ -26,7 +23,7 @@ public interface EmiStackConvertible {
 	 * @return The default representation of this type as an EmiStack.
 	 *  Uses the provided amount.
 	 */
-	@Environment(EnvType.CLIENT)
+
 	@ApiStatus.Experimental
 	default EmiStack emi(long amount) {
 		throw new IllegalStateException();
